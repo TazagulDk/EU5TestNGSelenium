1a8945c8c39d4c7a7d727cdd195203285d35a162
Tazagul Duishobai <kg.tazagul@gmail.com>
2021-06-17 23:33:29 +0200

    added

--- src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
+++ src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
@@ -29,6 +29,32 @@ public class TestNGAssertionsDemo {
         Assert.assertEquals("text", "text");
     }
 
+    @Test
+    public void test3(){
+        String expectedTitle = "Cyt";
+        String actualTitle = "Cybertek";
+
+        Assert.assertTrue(actualTitle.startsWith(expectedTitle),"Verify title starts with Cyb");
+
+    }
+
+    @Test
+    public void test4(){
+        //verify email contains @ sign
+        String email = "mikesmith.com";
+        Assert.assertTrue(email.contains("@"),"verify email contains @");
+    }
+
+    @Test
+    public void test5(){
+        Assert.assertFalse(0>1,"verify tha 0 is not greater than 1");
+    }
+
+    @Test
+    public void test6(){
+        Assert.assertNotEquals("one","two");
+    }
+
     @AfterMethod
     public void tearDown(){
         System.out.println("Close Browser");
